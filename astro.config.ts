adef6747b6d9ed9c06488403e383769985423b49
Cody Zuschlag <cody.zuschlag@gmail.com>
2025-04-15 16:04:28 +0200

    add mdx support

--- astro.config.ts
+++ astro.config.ts
@@ -2,11 +2,13 @@
 import {defineConfig} from 'astro/config';
 import react from '@astrojs/react';
 import unoCSS from 'unocss/astro';
+import mdx from '@astrojs/mdx';
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [
     react(),
+    mdx(),
     unoCSS({
       injectReset: '@unocss/reset/tailwind.css',
     }),
